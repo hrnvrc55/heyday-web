f24699bdeaebfa062325731eb7fe6cc56dc4432f
voxxNihili <4valerian@gmail.com>
2023-01-26 16:46:27 +0300

    fix404

---
+++ public/assets/js/precache-manifest.6715d655977bb42f24159c4b9813203a.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f3692b1794dc2850a2",
+    "url": "/assets/js/vendor.bundle.js"
+  },
+  {
+    "revision": "1e2a98366ebaf1b8892b",
+    "url": "/assets/js/app.bundle.js"
+  }
+];
