f78a7df9dd7ea0ba74f3c2fe23f954a6336f460f
voxxNihili <4valerian@gmail.com>
2023-02-08 10:32:57 +0300

    son push geri alındı

--- public/assets/js/app.bundle.js
+++ public/assets/js/app.bundle.js
@@ -12271,8 +12271,8 @@
                       PROJECT.screenFix.addNoKeyUp(),
                       PROJECT.screenFix.addNoWheel(),
                       "list" === PROJECT.worksMode
-                        ? ((PROJECT.pageTransitionState.worksDetailToTop = 1),
-                        i.a.triggerPageTransition(PROJECT.worksUrlList.cycle(this.info.path)))
+                        ? ((PROJECT.pageTransitionState.worksDetailToWorks = 1),
+                          i.a.triggerPageTransition("/"))
                         : ((PROJECT.pageTransitionState.worksDetailToTop = 1),
                         i.a.triggerPageTransition(PROJECT.worksUrlList.cycle(this.info.path)))))
                 },
